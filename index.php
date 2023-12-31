288119bce6c9eb4b9d5ba434b967b3856681c5a1
Victor de Albuquerque Xavier <victorxav.01@gmail.com>
2023-12-31 21:21:14 +0000

    update

--- index.php
+++ index.php
@@ -5,9 +5,69 @@ include "./php/cabecalho.php";
     <div class="banner">
 
     </div>
-    <div class="overflow">
+    <div class="pos-banner">
+        <form >
+            <fieldset>
+                <legend>sobre nois</legend>
+                <p>sdasdasdasdasdasdasdasd
+                    adsadasdasdasdasd
+                </p>
+            </fieldset>
+        </form>
+    </div>
+
+    <div class="fots-em-line">
+        <section>
+            <div class="foto ft1"></div>    
+            <div>
+                <p>
+                askdasdassd asdasd asdasd asd sadasdasd asd sda sdas da sdassdsd sdasdas dasdas dasdasd sadas das dasd asd asd asdas da sd      
+                </p>
+            </div>
+        </section>
+
+        <section>
+            <div class="foto ft1"></div>    
+            <div>
+                <p>
+                askdasdassd asdasd asdasd asd sadasdasd asd sda sdas da sdassdsd sdasdas dasdas dasdasd sadas das dasd asd asd asdas da sd      
+                </p>
+            </div>
+        </section>
+
 
     </div>
+    <div class="fots-em-line">
+
+        <section>
+            <div class="foto ft1"></div>    
+            <div>
+                <p>
+                askdasdassd asdasd asdasd asd sadasdasd asd sda sdas da sdassdsd sdasdas dasdas dasdasd sadas das dasd asd asd asdas da sd        
+                </p>
+            </div>
+        </section>
+
+        <section>
+            <div class="foto ft1"></div>    
+            <div>
+                <p>
+                askdasdassd asdasd asdasd asd sadasdasd asd sda sdas da sdassdsd sdasdas dasdas dasdasd sadas das dasd asd asd asdas da sd      
+                </p>
+            </div>
+        </section>
+
+        <section>
+            <div class="foto ft1"></div>    
+            <div>
+                <p>
+                askdasdassd asdasd asdasd asd sadasdasd asd sda sdas da sdassdsd sdasdas dasdas dasdasd sadas das dasd asd asd asdas da sd      
+                </p>
+            </div>
+        </section>
+
+
+    </div>  
     
 </body>
 </html>
